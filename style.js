a491c65e5558031d8e7e8be9be69a577af0d75db
ksindhu86 <sindhurikommineni@gmail.com>
2024-10-21 13:17:21 +0530

    login form

---
+++ style.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.sin').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+      });
+          
+  });
